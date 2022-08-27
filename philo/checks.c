ab253e8cf9d90c3cf8dd41e04f1e7af0288cddf4
Kolja Kleinschmidt <kkleinsc@c2s18d116.42wolfsburg.de>
2022-08-27 15:48:26 +0200

    added konstantins philos

---
+++ philo/checks.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checks.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkleinsc <kkleinsc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/06 16:03:25 by kmeixner          #+#    #+#             */
+/*   Updated: 2022/05/06 17:58:29 by kkleinsc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	check_exit(t_all *all)
+{
+	int	r;
+
+	pthread_mutex_lock(&(all->m_exit));
+	r = all->exit;
+	pthread_mutex_unlock(&(all->m_exit));
+	if (r)
+		return (1);
+	else
+		return (0);
+}
+
+int	checkmeals(t_all *all)
+{
+	t_phil	*tmp;
+
+	tmp = all->firstphil;
+	while (tmp)
+	{
+		pthread_mutex_lock(&(all->m_meals));
+		if (tmp->meals_eaten < all->meals)
+		{
+			pthread_mutex_unlock(&(all->m_meals));
+			return (0);
+		}
+		pthread_mutex_unlock(&(all->m_meals));
+		tmp = tmp->nextphil;
+	}
+	return (1);
+}
+
+void	checkroutine(t_all *all)
+{
+	t_phil		*phil;
+	t_timeval	now;
+	int			tdiff;
+
+	while (!(all->exit))
+	{
+		gettimeofday(&now, NULL);
+		phil = all->firstphil;
+		while (phil && !(all->exit))
+		{
+			pthread_mutex_lock(&(all->m_time));
+			tdiff = (now.tv_sec - phil->lastmeal.tv_sec) * 1000;
+			tdiff += (now.tv_usec - phil->lastmeal.tv_usec) / 1000;
+			pthread_mutex_unlock(&(all->m_time));
+			if (tdiff >= all->ttd)
+				phil_ded(all, phil);
+			phil = phil->nextphil;
+		}
+		if (all->meals > 0 && checkmeals(all))
+		{
+			pthread_mutex_lock(&(all->m_exit));
+			all->exit = 1;
+			pthread_mutex_unlock(&(all->m_exit));
+		}	
+		usleep(1000);
+	}
+}
