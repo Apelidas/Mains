c407cf1ac7d5aedf7c04134bbbd7a6c1b793e2dd
Kolja Kleinschmidt <kkleinsc@c2s19d123.42wolfsburg.de>
2022-12-11 17:48:40 +0100

    started module 6

---
+++ 06_tmp/ex00/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+
+
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+	if (argc < 2)
+	{
+		std::cout << "Wrong amount of Arguments" << std::endl;
+		return (1);
+	}
+
+	
+
+}
