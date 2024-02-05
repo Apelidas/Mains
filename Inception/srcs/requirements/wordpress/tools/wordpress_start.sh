8653ab6cde3ae295c4f8e6a28894e170495f2ee4
apelidas <kolja.kleinschmidt@gmx.de>
2024-02-05 18:44:58 +0100

    sdoluifh

--- Inception/srcs/requirements/wordpress/tools/wordpress_start.sh
+++ Inception/srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -8,12 +8,12 @@
 
 	echo "Wordpress: setting up..."
 	mkdir -p /var/www/html
-	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
-	mv wp-cli.phar /usr/bin/wp;
-	chmod +x /usr/bin/wp; 
+	# wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+	# mv wp-cli.phar /usr/bin/wp;
+	# chmod +x /usr/bin/wp; 
 	cd /var/www/html;
 	wp core download --allow-root;
-	mv /var/www/wp-config.php /var/www/html/
+	# mv /var/www/wp-config.php /var/www/html/
 	echo "Wordpress: creating users..."
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
