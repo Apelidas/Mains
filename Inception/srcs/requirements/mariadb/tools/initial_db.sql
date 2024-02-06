9cb8f442cbd120ee46c742cc6f84f44dd4d547be
apelidas <kolja.kleinschmidt@gmx.de>
2024-02-06 16:50:33 +0100

    move to VM

--- Inception/srcs/requirements/mariadb/tools/initial_db.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'kkleinsc'@'localhost' IDENTIFIED BY 'kkleinsc';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'kkleinsc'@'localhost';
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'kkleinsc';
