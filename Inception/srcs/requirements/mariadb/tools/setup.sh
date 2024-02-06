4af24e3cf18241daa0b3d7d04402e26b547609ee
apelidas <kolja.kleinschmidt@gmx.de>
2024-02-06 15:06:59 +0100

    move to VM

---
+++ Inception/srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,11 @@
+mkdir -p /var/run/mysqld
+chown mysql:mysql /var/run/mysqld
+chmod 755 /var/run/mysqld
+
+service mysql start
+
+mysql -u root -e CREATE DATABASE IF NOT EXISTS wordpress;
+mysql -u root -e CREATE USER IF NOT EXISTS 'kkleinsc'@'localhost' IDENTIFIED BY 'kkleinsc';
+mysql -u root -e GRANT ALL PRIVILEGES ON wordpress.* TO 'kkleinsc'@'localhost';
+mysql -u root -e FLUSH PRIVILEGES;
+
